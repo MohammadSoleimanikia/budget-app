539ab88ec3657b30e891ca77becd256ba2936905
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2025-10-30 21:55:04 +0330

    add confirm before deleting the budget .

--- src/components/ShowExpensesModal.tsx
+++ src/components/ShowExpensesModal.tsx
@@ -9,7 +9,8 @@ import {
 import { useBudgets } from "@/contexts/BudgetContexts";
 import Button from "./ui/Button";
 export default function ShowExpensesModal({ budgetId }: { budgetId: string }) {
-    const { getBudgetExpenses, budgets,deleteBudget,deleteExpense } = useBudgets();
+    const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
+        useBudgets();
     const budgetExpenses = getBudgetExpenses(budgetId);
     const budget = budgets.find((budget) => budget.id === budgetId);
     return (
@@ -19,24 +20,41 @@ export default function ShowExpensesModal({ budgetId }: { budgetId: string }) {
             </DialogTrigger>
             <DialogContent className="bg-white">
                 <DialogHeader>
-                    <div className="flex justify-end items-baseline gap-2">
-                        <Button variant="remove" onClick={()=>deleteBudget(budgetId)}>حذف</Button>
+                    <div className="flex justify-end items-baseline gap-2 mt-3 sm:mt-0">
+                        <Button
+                            variant="remove"
+                            onClick={() => {
+                                if (confirm("آیا مطمئنی میخوای بودجه رو پاک کنی ؟")) deleteBudget(budgetId);
+                            }}
+                        >
+                            حذف
+                        </Button>
                         <DialogTitle>
                             مشاهده هزینه های {budget?.name}
                         </DialogTitle>
                     </div>
                     <DialogDescription className="flex flex-col gap-2 text-base">
                         {budgetExpenses.map((expense) => {
-                            return <div className="flex bg items-baseline rounded bg-gray-100 p-1 px-2 justify-between" key={expense.id}>
-                                <div className="flex gap-2 items-baseline">
-                                    <Button variant="remove" onClick={()=>deleteExpense(expense.id)}> x </Button>
-                                    <p>{expense.amount}</p>
+                            return (
+                                <div
+                                    className="flex bg items-baseline rounded bg-gray-100 p-1 px-2 justify-between"
+                                    key={expense.id}
+                                >
+                                    <div className="flex gap-2 items-baseline">
+                                        <Button
+                                            variant="remove"
+                                            onClick={() =>
+                                                deleteExpense(expense.id)
+                                            }
+                                        >
+                                            {" "}
+                                            x{" "}
+                                        </Button>
+                                        <p>{expense.amount}</p>
+                                    </div>
+                                    <h2>{expense.description}</h2>
                                 </div>
-                                <h2>
-                                    {expense.description}
-                                </h2>
-
-                            </div>;
+                            );
                         })}
                     </DialogDescription>
                 </DialogHeader>
